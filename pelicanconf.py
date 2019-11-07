a69fdd5dbda7046faebc4f630f47b8190ccd2a1a
40823143 <40823143@gm.nfu.edu.tw>
2019-11-07 12:42:24 +0800

    123

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40823143'
+SITENAME = '40823143機算機程式cp2019'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
